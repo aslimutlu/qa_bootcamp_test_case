2b40439066e60d1852c7a5adf4ecdf8ae889c07d
Aslı Mutlu <32126870+aslimutlu@users.noreply.github.com>
2024-03-24 20:33:21 +0300

    Update automation_run.groovy

--- tests/jenkins/scripts/automation_run.groovy
+++ tests/jenkins/scripts/automation_run.groovy
@@ -4,6 +4,7 @@ pipeline {
         DEVELOPER = 'develop'
         BRANCH = "${BRANCH_NAME}" 
         ANSIBLE_FORCE_COLOR = 'true'
+        REQUIREMENTS_FILE_PATH = "C:\\Users\\aslim\\OneDrive\\Belgeler\\GitHub\\qa_bootcamp_test_case\\tests\\requirements.txt"
     }
 
     stages {
@@ -18,9 +19,9 @@ pipeline {
         stage('Automation Process') {
             steps {
                 powershell '''
-                    Set-Location -Path "C:\\User\\aslim\\.jenkins\\workspace\\qa_pipeline"
+                    Set-Location -Path "C:\\Users\\aslim\\OneDrive\\Belgeler\\GitHub\\qa_bootcamp_test_case\\tests"
                     Write-Output "Automation process running..."
-                    python -m pip install -r requirements.txt
+                    python -m pip install -r $env:REQUIREMENTS_FILE_PATH
                     Get-Location
                 '''
             }
