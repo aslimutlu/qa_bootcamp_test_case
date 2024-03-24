bf044c2181612c5d047221993617ab8076093cca
Aslı Mutlu <32126870+aslimutlu@users.noreply.github.com>
2024-03-24 20:22:55 +0300

    Update automation_run.groovy

--- tests/jenkins/scripts/automation_run.groovy
+++ tests/jenkins/scripts/automation_run.groovy
@@ -2,31 +2,29 @@ pipeline {
     agent any
     environment {
         DEVELOPER = 'develop'
-        BRANCH = '${BRANCH_NAME}' 
+        BRANCH = "${BRANCH_NAME}" 
         ANSIBLE_FORCE_COLOR = 'true'
-
     }
 
-    stages{
-        stage('Get branch name'){
-            steps{
+    stages {
+        stage('Get branch name') {
+            steps {
                 script {
                     branch = env.BRANCH
                 }
             }
         }
     
-
-        stage('Automation Process'){
-            script{
-                dir('/User/aslim/.jenkins/workspace/qa_pipeline'){
-                    echo 'Automation process running...'
+        stage('Automation Process') {
+            steps {
+                script {
+                    dir('/User/aslim/.jenkins/workspace/qa_pipeline') {
+                        echo 'Automation process running...'
                         sh 'pip3 install -r requirements.txt'
                         sh 'pwd'
+                    }
                 }
             }
         }
-
     }
-
 }
